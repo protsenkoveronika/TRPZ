e5836fc80b463e0c4c0ebc67a307ad55300652de
Veronika Protsenko <protsenko.veronika@lll.kpi.ua>
2024-11-27 20:43:28 +0100

    Modified lab4

--- system-activity-monitor/main.py
+++ system-activity-monitor/main.py
@@ -1,23 +1,10 @@
-from repositories.monitor_repository import MonitorRepository
-from repositories.memory_repository import MemoryRepository
-from memory_usage import MemoryUsage
-
+from activity_monitor import ActivityMonitor
 
 def main():
     db_file = 'db.sqlite'
 
-    monitor_repo = MonitorRepository(db_file)
-    monitor_repo.insert_current_date()
-    monitor_repo.close()
-
-    memory_usage_monitor = MemoryUsage(db_file)
-    memory_usage_monitor.first_check_and_monitoring()
-    
-
-    # Other initialization code...
-
-    # Close the database connection when done
-    memory_usage_monitor.close()
+    monitor = ActivityMonitor(db_file)
+    monitor.start_gui()
     
 
 if __name__ == "__main__":
